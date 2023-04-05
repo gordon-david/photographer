0680f736f9ed6d4f2fca947c7ad3dc3316580bef
david <d.phi.gordon@gmail.com>
2023-04-04 20:19:41 -0500

    removed small, unused css class

--- frontend/src/components/NavLink.tsx
+++ frontend/src/components/NavLink.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 
   export const NavLink = (props: { path:string, text:string }) => (
-    <Link to={props.path} className="px-4 font-bold text-lg hover:text-red-500 red">
+    <Link to={props.path} className="px-4 font-bold text-lg hover:text-red-500">
       {`${props.text}`}
     </Link>
   );
